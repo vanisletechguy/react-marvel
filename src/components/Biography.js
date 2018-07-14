a7c6b0225d3975d933c53b353dbb6693d7065a22
mikenic <temperanceaubie@gmail.com>
2018-07-14 14:02:16 -0700

    refactor

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -4,15 +4,15 @@ import {connect} from 'react-redux';
 
 class Biography extends Component {
 	render() {
-		if(this.props.selectedCharacter[1]){
-			this.current = this.props.selectedCharacter[1];
+		if(this.props.selectedCharacter){
+			this.current = this.props.selectedCharacter;
 		}
 		return(
 			<div>
 				{
-					this.current ?
+					this.current.name?
 						<div className="card">
-							<h2 className="card-title">{this.current.name}</h2>
+							<h3>{this.current.name}</h3>
 							<p className="card-text">{this.current.description}</p>
 							<img className="thumbnail" 
 								src={this.current.thumbnail.path + 
@@ -29,7 +29,7 @@ class Biography extends Component {
 
 function mapStateToProps(state){
 	return {
-		selectedCharacter: state.characters
+		selectedCharacter: state.selected
 	};
 }
 export default connect(mapStateToProps, null)(Biography);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,16 +4,27 @@ import {RECEIVE_CHARACTERS, SET_SELECTED_CHARACTER} from '../actions';
 function characters(state = [], action) {
 	switch (action.type) {
 	case RECEIVE_CHARACTERS:
-		state = {};		
-		state = [...state, action.characters];
+		//state = {};		
+		//state = [...state, action.characters];
+		state = [action.characters];
 		return state;
-	case SET_SELECTED_CHARACTER:
-		state.character = [];
-		if(state[1]){state.pop();}
-		return [...state, action.character];
+//	case SET_SELECTED_CHARACTER:
+//		if(state[1]){state.pop();}
+//		return [...state, action.character];
 	default:
 		return state;	
 	}
 }
-const rootReducer = combineReducers({characters});
+
+
+function selected(state = [], action) {
+	switch (action.type) {
+	case SET_SELECTED_CHARACTER:
+		state = action.character;
+		return state;
+	default:
+		return state;
+	}
+}
+const rootReducer = combineReducers({characters, selected});
 export default rootReducer;
